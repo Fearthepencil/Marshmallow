c48f69e8dd88edbe5c595b60c7fd88cd643a580c
Pavle Stefanovic <pavles.dev@gmail.com>
2026-01-21 20:48:14 +0100

    pocetak kotlin vezbanje

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+
+fun main(){
+    val name: String = "kurac"
+    var var_name: String = "promenljivo"
+    var_name = "kurcina"
+    val flag: Boolean = false
+    println(var_name)
+    println(name)
+    println(flag)
+}
+
+fun var_decider(){
+
+}
