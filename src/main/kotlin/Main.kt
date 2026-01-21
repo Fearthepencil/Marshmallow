abd06a9a8e98afc9289553af061d194d97fc85da
Pavle Stefanovic <pavles.dev@gmail.com>
2026-01-21 21:01:41 +0100

    new learning

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,13 @@ fun main(){
     var var_name: String = "promenljivo"
     var_name = "kurcina"
     val flag: Boolean = false
-    println(var_name)
-    println(name)
-    println(flag)
+    println("Just learned a better way to print $name $var_name $flag")
+    val x:Float = 4F
+    println(x)
+    print("Input a number so I can tell you if it is even: ")
+    val y = readln().toInt()
+    val isEven = y%2 == 0
+    println("Even is (true/false): $isEven")
 }
 
 fun var_decider(){
