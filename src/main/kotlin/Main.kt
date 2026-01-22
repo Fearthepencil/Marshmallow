6c8df88449ffb83e5ca624576098715231ba1556
Pavle Stefanovic <pavles.dev@gmail.com>
2026-01-22 11:19:29 +0100

    new learning

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,16 @@
 
 fun main(){
-    val name: String = "kurac"
-    var var_name: String = "promenljivo"
-    var_name = "kurcina"
-    val flag: Boolean = false
-    println("Just learned a better way to print $name $var_name $flag")
-    val x:Float = 4F
-    println(x)
-    print("Input a number so I can tell you if it is even: ")
-    //Safe - works
-    val y = readln().toIntOrNull()?.inc()
+    val y = readln().toIntOrNull()?.inc()?.dec()
     val isEven = y?.rem(2) == 0
-    println("Even? $isEven")
+    //you can use ifs as conditions
+    val output = if(isEven){
+        "even"
+    } else {
+        "odd"
+    }
+    println("The number you entered is: $output")
 }
+
 
 fun var_decider(){
 
