cec2e8cac8a934a57d7c398dd44fe903d89bc595
Fearthepencil <pavles.dev@gmail.com>
2026-02-08 00:57:44 +0100

    Nullable, null-safe operations

--- src/Main.kt
+++ src/Main.kt
@@ -29,6 +29,24 @@ fun main() {
     println("Uneli ste $input")
 
     /**
-     * NULLABILITY -
+     * NULLABILITY - obicni tipovi NE MOGU da imaju NULL KAO VREDNOST, po defaultu su NULL-SAFE.
+     * Ako je Int null-safe tip, Int? je nullable tip - u ovakav tip se moze upisati vrednost null.
+     * Posto npr. input moze biti null, mora se ovde staviti ILI ternarni operator i obicna moduo operacija
+     * ILI forceovati da prodje koristeci !! npr: Int? is x!!%2==0 - ovo nije dobro jer tehnicki i dalje moze da bude null
+     * pa da baci gresku. Zato koristimo ovde ternarni operator - ako je null dodelice se 0 u x.
      */
+    println("Enter a variable to see if it is even")
+    val x: Int = readln().toIntOrNull()?:0
+    val isEven = x%2==0
+    println("The variable you is even: $isEven")
+
+    /**
+     * Kotlin f-je imaju NULL-SAFE pozive - f-je ce se pozvati nad vrednoscu SAMO AKO NIJE NULL
+     * Postoje f-je umesto vecine operatora - npr. ++ - inc, % -- rem(mod).
+     * Poenta ovoga je da ne moras da koristis ternarne operatore pri dodeli kao npr gore
+     */
+    println("Enter a variable to see if it is even - nullsafe")
+    val z: Int? = readln().toIntOrNull()?.inc()
+    val isEvenZ = z?.rem(2)
+    println("The variable incremented is: $z and it is now even: $isEvenZ")
 }
